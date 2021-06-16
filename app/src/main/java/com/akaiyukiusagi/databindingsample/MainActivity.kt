6b76a7a5a7f4445791cb87c27cefae5075a98990
shuhei-ff <s.kiyosawa@frontierfield.co.jp>
2021-06-16 18:00:21 +0900

    とりあえず動く

--- app/src/main/java/com/akaiyukiusagi/databindingsample/MainActivity.kt
+++ app/src/main/java/com/akaiyukiusagi/databindingsample/MainActivity.kt
@@ -2,10 +2,24 @@ package com.akaiyukiusagi.databindingsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import com.akaiyukiusagi.databindingsample.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewModel: ViewModel by viewModels()
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        button.setOnClickListener {
+            viewModel.text.value = editText.text.toString()
+        }
     }
 }
---
+++ app/src/main/java/com/akaiyukiusagi/databindingsample/ViewModel.kt
@@ -0,0 +1,19 @@
+package com.akaiyukiusagi.databindingsample
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ViewModel : ViewModel() {
+    val text: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    init{
+        text.value = "初期値"
+    }
+
+    fun applyText() {
+
+    }
+}
+
